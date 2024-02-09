af8e8fdc4c030713f6d5df1df46e4319403793a6
Chanakya <sunkarapallychanakya@gmail.com>
2024-02-09 19:42:19 +0530

    initial commit

---
+++ src/main/java/spring/io/rest/insaneresourceprovider/InsaneResourceProviderApplication.java
@@ -0,0 +1,13 @@
+package spring.io.rest.insaneresourceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsaneResourceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsaneResourceProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/io/rest/insaneresourceprovider/InsaneResourceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.io.rest.insaneresourceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsaneResourceProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
